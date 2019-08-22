84fd6c7e0ddbaaebbde452f455868fd6ec789726
tejaswigowda <tejaswi@foxyninjastudios.com>
2019-08-22 11:12:00 -0700

    ac

--- res/js/assetninja.xyz-tool.js
+++ res/js/assetninja.xyz-tool.js
@@ -50,55 +50,52 @@ function loadImage()
               opt.e.preventDefault();
               opt.e.stopPropagation();
             });
-
             canvas.on('mouse:down', function(o){
-              if(canvas.getActiveObject() != null || canvas.getActiveObject() != undefined) return;
-                isDown = true;
-                var pointer = canvas.getPointer(o.e);
-                origX = pointer.x;
-                origY = pointer.y;
-                var pointer = canvas.getPointer(o.e);
-                rect = new fabric.Rect({
-                    left: origX,
-                    top: origY,
-                   // originX: 'left',
-                  //  originY: 'top',
-                    width: pointer.x-origX,
-                    height: pointer.y-origY,
-                    angle: 0,
-                    stroke: 'rgba(0,0,255,1)',
-                    strokeWidth: 3,
-                    fill: 'rgba(255,0,0,0.1)',
-                    selectable: true,
-                    evented: true,
-                    transparentCorners:true 
-                });
-                canvas.add(rect);
-            });
+              if (canvas.getActiveObject())
+                  return;
+              isDown = true;
+              var pointer = canvas.getPointer(o.e);
+              origX = pointer.x;
+              origY = pointer.y;
+              var pointer = canvas.getPointer(o.e);
+              rect = new fabric.Rect({
+                  left: origX,
+                  top: origY,
+                  originX: 'left',
+                  originY: 'top',
+                  width: pointer.x-origX,
+                  height: pointer.y-origY,
+                  angle: 0,
+                  fill: 'rgba(255,0,0,0.5)',
+                  transparentCorners: false
+              });
+              canvas.add(rect);
+          });
 
-            canvas.on('mouse:move', function(o){
-                if (!isDown) return;
-              if(canvas.getActiveObject() != null || canvas.getActiveObject() != undefined) return;
-                var pointer = canvas.getPointer(o.e);
+          canvas.on('mouse:move', function(o){
+              if (!isDown) return;
+              var pointer = canvas.getPointer(o.e);
 
-                if(origX>pointer.x){
-                    rect.set({ left: Math.abs(pointer.x) });
-                }
-                if(origY>pointer.y){
-                    rect.set({ top: Math.abs(pointer.y) });
-                }
+              if(origX>pointer.x){
+                  rect.set({ left: Math.abs(pointer.x) });
+              }
+              if(origY>pointer.y){
+                  rect.set({ top: Math.abs(pointer.y) });
+              }
 
-                rect.set({ width: Math.abs(origX - pointer.x) });
-                rect.set({ height: Math.abs(origY - pointer.y) });
+              rect.set({ width: Math.abs(origX - pointer.x) });
+              rect.set({ height: Math.abs(origY - pointer.y) });
+              console.log(pointer);
 
+              canvas.renderAll();
+          });
 
-                canvas.renderAll();
-            });
-
-            canvas.on('mouse:up', function(o){
-              isDown = false;
-            });
-
+          canvas.on('mouse:up', function(o){
+              if (isDown) {
+                rect.setCoords();
+                isDown = false;
+              }
+          });
         }
         img.src = data;
 
